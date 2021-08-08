aedd224e4d4d06551996607217385becd4911ad2
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-08-08 19:57:45 -0300

    Add basic testing framework

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,78 @@
 #![no_std] // don't link the Rust standard Library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+mod serial;
 mod vga_buffer;
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] //don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    println!("Hello again");
-    println!(", some numbers: {} {}", 42, 1.337);
-    println!("One \n Two\nTree\nFour");
+    println!("Hello world");
 
-    panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-// This function is called on panic
+// ----------------------------------------------------------------------
+// Panic Functionality
+// ----------------------------------------------------------------------
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("{}", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+// ----------------------------------------------------------------------
+// Testing Functionality
+// ----------------------------------------------------------------------
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    // new
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trival_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
+}
+
+// ----------------------------------------------------------------------
+// Exit Functionality
+// ----------------------------------------------------------------------
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
